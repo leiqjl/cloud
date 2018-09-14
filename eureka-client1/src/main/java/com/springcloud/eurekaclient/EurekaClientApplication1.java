d1c0b350ecd530818bb84d7ab524d41fc8ccf5ab
qijl <leiqjl@163.com>
2018-09-14 19:45:34 +0800

    init

---
+++ eureka-client1/src/main/java/com/springcloud/eurekaclient/EurekaClientApplication1.java
@@ -0,0 +1,14 @@
+package com.springcloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication1.class, args);
+    }
+}
