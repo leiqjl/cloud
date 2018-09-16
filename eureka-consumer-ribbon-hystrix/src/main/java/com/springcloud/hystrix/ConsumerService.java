528b0debe12e4be23751231c495a2097929045f3
leiqjl <leiqjl@gmail.com>
2018-09-16 13:48:40 +0800

    hystrix init

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/springcloud/hystrix/ConsumerService.java
@@ -0,0 +1,22 @@
+package com.springcloud.hystrix;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ConsumerService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public String consumer() {
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+    public String fallback() {
+        return "fallback";
+    }
+}
